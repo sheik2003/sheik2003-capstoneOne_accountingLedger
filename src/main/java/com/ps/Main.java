844286ded7641ff4a3781ce6395287f5ca6d24fc
Sheik siphat <notsheiksiphat2003@gmail.com>
2025-04-28 12:13:53 -0400

    created the skeleton for homescreen and also added transaction parser and adding transaction to arraylist

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,16 +1,90 @@
 package com.ps;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Transaction> transactions = new ArrayList<>();
 
-
+    //main
     public static void main(String[] args) {
 
 
+        int homeScreenCommand;
 
+        do {
+            System.out.println("Welcome to the home screen");
+            System.out.println("1) Add Deposit");
+            System.out.println("2) Make Payment (Debit)");
+            System.out.println("3) Ledger");
+            System.out.println("0) Exit");
+            System.out.println("What would you like to do? ");
+            homeScreenCommand = scanner.nextInt();
+
+            switch (homeScreenCommand){
+                case 1:
+                    addDeposit();
+                    break;
+                case 2:
+                    makePayment();
+                    break;
+                case 3:
+                    displayLedger();
+                    break;
+                case 0:
+                    System.out.println("Exiting");
+                    break;
+                default:
+                    System.out.println("Wrong command entered please retry again with a valid choice ");
+
+            }
+
+        }
+        while (homeScreenCommand != 0);
     }
+
+    private static void displayLedger() {
+    }
+
+    private static void makePayment() {
+    }
+
+    private static void addDeposit() {
+    }
+
+    private static void loadTransactions() {
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("transactions.csv"));
+
+            String input;
+
+            while ((input = bufferedReader.readLine()) != null){
+                String[] fields = input.split("\\|");
+
+                LocalDate date = LocalDate.parse(fields[0]);
+                LocalTime time = LocalTime.parse(fields[1]);
+                String description = fields[2];
+                String vendor = fields[3];
+                double amount = Double.parseDouble(fields[4]);
+
+                Transaction transaction = new Transaction(date,time,description,vendor,amount);
+
+                transactions.add(transaction);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
 }
 
-//capstoneOne_accountingLedger
